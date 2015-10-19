7ac44fc3b73183b0cce810a0dafcf6d286be8b0e
k4s <624076768@qq.com>
2015-10-19 16:13:08 +0800

    填加执行程序，动态修改执行路径
    填加执行程序动态修改执行路径

--- phantom.go
+++ phantom.go
@@ -11,15 +11,20 @@ var GOPATH = os.Getenv("GOPATH")
 
 type Phantomer interface {
 	SetUserAgent(string)
+	SetFilePath(string, string)
 	Start(args []string) (io.ReadCloser, error)
 }
 
 type Phantom struct {
 	userAgent string
+	filePath  map[string]string
 }
 
 func NewPhantom() Phantomer {
-	phantom := &Phantom{}
+	phantom := &Phantom{filePath: map[string]string{
+		"phantomjs": GOPATH + "/src/github.com/k4s/phantomgo/phantomjs/phantomjs",
+		"openjs":    GOPATH + "/src/github.com/k4s/phantomgo/js/open.js",
+	}}
 	return phantom
 }
 
@@ -33,7 +38,7 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 			}
 			return result, err
 		} else if args[0] == "open" {
-			result, err = self.Open(GOPATH+"/src/github.com/k4s/phantomgo/js/open.js", args[1])
+			result, err = self.Open(self.filePath["openjs"], args[1])
 			if err != nil {
 				return nil, err
 			}
@@ -49,13 +54,13 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 				//args[1] 为请求地址
 				//args[2] 为页面编码
 				//userAgent 为客户端代理请求设备，默认为百度爬虫
-				result, err := self.Open(GOPATH+"/src/github.com/k4s/phantomgo/js/open.js", args[1], args[2], self.userAgent)
+				result, err := self.Open(self.filePath["openjs"], args[1], args[2], self.userAgent)
 				if err != nil {
 					return nil, err
 				}
 				return result, err
 			} else {
-				result, err := self.Open(GOPATH+"/src/github.com/k4s/phantomgo/js/open.js", args[1], args[2])
+				result, err := self.Open(self.filePath["openjs"], args[1], args[2])
 				if err != nil {
 					return nil, err
 				}
@@ -67,7 +72,7 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 }
 
 func (self *Phantom) Open(openArgs ...string) (stdout io.ReadCloser, err error) {
-	cmd := exec.Command("phantomjs", openArgs...)
+	cmd := exec.Command(self.filePath["phantomjs"], openArgs...)
 	stdout, err = cmd.StdoutPipe()
 	if err != nil {
 		return nil, err
@@ -85,4 +90,9 @@ func (self *Phantom) Exec(js string) (stdout io.ReadCloser, err error) {
 
 func (self *Phantom) SetUserAgent(userAgent string) {
 	self.userAgent = userAgent
+}
+
+// 动态修改执行文件路径
+func (self *Phantom) SetFilePath(name string, filepath string) {
+	self.filePath[name] = filepath
 }
