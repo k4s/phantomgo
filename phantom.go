8c6517f31c109fea89f6619eb1a53870f65d1a80
k4s <624076768@qq.com>
2015-10-22 17:43:50 +0800

    Revert "更改js执行路径"
    This reverts commit b929c680413a313f646e1b4b7b837d888d0fda6b.

--- phantom.go
+++ phantom.go
@@ -7,29 +7,24 @@ import (
 	"os/exec"
 )
 
-//javascript temp file name
-const JS_FILE_NAME = "tem_js_to_phantom"
-
 var GOPATH = os.Getenv("GOPATH")
 
 type Phantomer interface {
 	SetUserAgent(string)
-	SetPhantomjsPath(string, string)
+	SetFilePath(string, string)
 	Start(args []string) (io.ReadCloser, error)
 }
 
 type Phantom struct {
-	userAgent     string
-	jsFileName    string
-	phantomjsPath string
+	userAgent string
+	filePath  map[string]string
 }
 
 func NewPhantom() Phantomer {
-	phantom := &Phantom{
-		jsFileName:    JS_FILE_NAME,
-		phantomjsPath: GOPATH + "/src/github.com/k4s/phantomgo/phantomjs/phantomjs",
-	}
-	phantom.CreatJsFile()
+	phantom := &Phantom{filePath: map[string]string{
+		"phantomjs": GOPATH + "/src/github.com/k4s/phantomgo/phantomjs/phantomjs",
+		"openjs":    GOPATH + "/src/github.com/k4s/phantomgo/js/open.js",
+	}}
 	return phantom
 }
 
@@ -43,7 +38,7 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 			}
 			return result, err
 		} else if args[0] == "open" {
-			result, err = self.Open(self.jsFileName, args[1])
+			result, err = self.Open(self.filePath["openjs"], args[1])
 			if err != nil {
 				return nil, err
 			}
@@ -59,13 +54,13 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 				//args[1] 为请求地址
 				//args[2] 为页面编码
 				//userAgent 为客户端代理请求设备，默认为百度爬虫
-				result, err := self.Open(self.jsFileName, args[1], args[2], self.userAgent)
+				result, err := self.Open(self.filePath["openjs"], args[1], args[2], self.userAgent)
 				if err != nil {
 					return nil, err
 				}
 				return result, err
 			} else {
-				result, err := self.Open(self.jsFileName, args[1], args[2])
+				result, err := self.Open(self.filePath["openjs"], args[1], args[2])
 				if err != nil {
 					return nil, err
 				}
@@ -76,9 +71,8 @@ func (self *Phantom) Start(args []string) (result io.ReadCloser, err error) {
 	return nil, errors.New("args error")
 }
 
-//打开远程地址
 func (self *Phantom) Open(openArgs ...string) (stdout io.ReadCloser, err error) {
-	cmd := exec.Command(self.phantomjsPath, openArgs...)
+	cmd := exec.Command(self.filePath["phantomjs"], openArgs...)
 	stdout, err = cmd.StdoutPipe()
 	if err != nil {
 		return nil, err
@@ -90,34 +84,15 @@ func (self *Phantom) Open(openArgs ...string) (stdout io.ReadCloser, err error) 
 	return stdout, err
 }
 
-//预留方法
 func (self *Phantom) Exec(js string) (stdout io.ReadCloser, err error) {
 	return stdout, err
 }
 
-//设置本地代理客户端
 func (self *Phantom) SetUserAgent(userAgent string) {
 	self.userAgent = userAgent
 }
 
 // 动态修改执行文件路径
-func (self *Phantom) SetPhantomjsPath(name string, filepath string) {
-	self.phantomjsPath = filepath
-}
-
-//创建js临时文件
-func (self *Phantom) CreatJsFile() {
-	js := loadjs()
-	_, err := os.Stat(self.jsFileName)
-	if err == nil || os.IsExist(err) {
-		return
-	} else {
-		file, _ := os.Create(self.jsFileName)
-		file.WriteString(js)
-	}
-
-}
-
-func (self *Phantom) DestroyJsFile() {
-	os.Remove(self.jsFileName)
+func (self *Phantom) SetFilePath(name string, filepath string) {
+	self.filePath[name] = filepath
 }
